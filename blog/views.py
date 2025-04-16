511012f8e97940cf113bfaa21cd36c26c2fa3c41
Adán Estévez <adanescollante@gmail.com>
2025-04-16 08:26:11 -0400

    add tailwind

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
-def home(request):
-    return HttpResponse("Hello world!")
+def landing(request):
+    template = loader.get_template("landing.html")
+    return HttpResponse(template.render())
