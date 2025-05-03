ba19e446d096c51265de9f456dbd17a14897a512
Adán Estévez <adanescollante@gmail.com>
2025-05-03 13:14:28 -0400

    built website base

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
 
 def landing(request):
-    template = loader.get_template("landing.html")
-    return HttpResponse(template.render())
+    return render(request, "landing.html")
---
+++ static/src/main.js
@@ -0,0 +1,6 @@
+navButton = document.getElementById('navButton');
+navMenu = document.getElementById('navMenu');
+
+navButton.addEventListener('click', () => {
+  navMenu.classList.toggle('hidden');
+});
