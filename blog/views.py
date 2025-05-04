5c45d4a2f5cf1f0da777fbb4a23c8182746c394f
Adán Estévez <adanescollante@gmail.com>
2025-05-04 15:16:00 -0400

    frontend prototype

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, "landing.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
+
+
+def post(request):
+    return render(request, "post.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
